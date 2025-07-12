5de1e03a91dc945887ebb751a40741ad9bb8579a
Tristan Morrison <tmorrison@esri.com>
2025-07-12 12:40:11 -0400

    Fix lima.zsh path variable

--- zsh/custom/lima.zsh
+++ zsh/custom/lima.zsh
@@ -1,14 +1,15 @@
 alias nerd="lima nerdctl"
+alias nerdctl="lima nerdctl"
 
 function start-local-apache() {
     echo 🔵 Starting local Apache server
-    HOME_ABSOLUTE = $(realpath "${HOME}")
+    HOME_ABSOLUTE=$(realpath "${HOME}")
     limactl start default
     lima nerdctl rm apache
     lima nerdctl run -d \
       --name apache \
       -p 127.0.0.1:80:80 \
       -p 127.0.0.1:443:443 \
-      --mount type=bind,source="${HOME_ABSOLUTE}/site/esri",target=/usr/local/apache2/htdocs/site/esri,rw \
+      --mount type=bind,source=$HOME_ABSOLUTE/site/esri,target=/usr/local/apache2/htdocs/site/esri,rw \
       apache_local
 }
